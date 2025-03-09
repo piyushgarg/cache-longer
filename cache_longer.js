5849caf235ae16922cee0be9f6c87d7841cdf8c3
Piyush <piyushgarg80>
2025-03-09 12:42:15 +0530

    ignore if it is already in cache, speed up variables in global scope, to avoid creating everytime.

--- cache_longer.js
+++ cache_longer.js
@@ -1,35 +1,36 @@
+// https://www.kishorenewton.com/posts/http_headers_deep_dive_boosting_efficiency_with_cache_control_etag_and_last_modified/
+
 console.log("Loading Cache Longer");
 
+var currentDate = new Date();
+currentDate.setMonth(currentDate.getMonth() + 6);
+new_expires = currentDate.toUTCString();  //6 months in the future
+var maxAge = 'public, max-age=15780000'; //6 months in seconds
 
 browser.webRequest.onHeadersReceived.addListener(
-  function(details) {
-  	var expires_found = false;
-  	var cache_control_found = false;
-    var d = new Date();
-    d.setMonth(d.getMonth() + 6);
-    new_expires = d.toUTCString();  //6 months in the future
-    for (var header of details.responseHeaders) {
-      if (header.name.toLowerCase() === 'expires') {
-        header.value = new_expires;
-        expires_found = true;
-      }
-      if (header.name.toLowerCase() === 'cache-control') {
-      	header.value = 'public, max-age=15780000'; //6 months
-      	cache_control_found = true;
-      }
-    }
-    if(!expires_found) {
-    	// If we are here, we didn't find any existing matching header.
-    	details.responseHeaders.push({name: "Expires", value: new_expires });
-    }
-    if(!cache_control_found) {
-    	details.responseHeaders.push({name: "Cache-Control", value: 'public, max-age=15780000' });	
-    }
-
-      
-    
-    return { responseHeaders: details.responseHeaders };
-  },
-  { urls: ["<all_urls>"], types: ["font", "image", "script", "stylesheet"] },
-  ["blocking", "responseHeaders"]
+    function (details) {
+        if (!details.fromCache) {
+            var expires_found = false;
+            var cache_control_found = false;
+            for (var header of details.responseHeaders) {
+                if (header.name.toLowerCase() === 'expires') {
+                    header.value = new_expires;
+                    expires_found = true;
+                } else if (header.name.toLowerCase() === 'cache-control') {
+                    header.value = maxAge;
+                    cache_control_found = true;
+                }
+            }
+            if (!expires_found) {
+                // If we are here, we didn't find any existing matching header.
+                details.responseHeaders.push({name: "Expires", value: new_expires});
+            }
+            if (!cache_control_found) {
+                details.responseHeaders.push({name: "Cache-Control", value: maxAge});
+            }
+        }
+        return {responseHeaders: details.responseHeaders};
+    },
+    {urls: ["<all_urls>"], types: ["font", "image", "script", "stylesheet"]},
+    ["blocking", "responseHeaders"]
 );
